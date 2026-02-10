3e21b3b1f73f0415cbd198a436412dfee5284252
Michael Nobles <Mnobles33@gmail.com>
2026-02-10 08:29:42 -0700

    Fix landing page layout, header/footer, and image constraints

--- src/components/layout/Footer.jsx
+++ src/components/layout/Footer.jsx
@@ -4,16 +4,16 @@ export default function Footer() {
   return (
     <footer className='flex-c w-[100%] primary-c justify-between px-14 text-amber-50'>
       <section className='footer-details flex pt-4 pb-8 relative'>
-        <div className='flex justify-between absolute top-4 left-0'>
+        <div className='flex items-start gap-4'>
           <a href='https://www.humanrightsfirst.org/'>
-            <img className='w-[100px]' src={Logo} alt='HRF logo white' />
+            <img className='h-10 w-auto shrink-0' src={Logo} alt='HRF logo white' />
           </a>
         </div>
-        <div className='footer-address flex-c-1 pt-4'>
-          <p>Human Rights First</p>
-          <p>75 Broad St, 31st Floor</p>
+        <div className='text-white/90'>
+          <p className='font-semibold'>Human Rights First</p>
+          <p className='mt-2'>75 Broad St, 31st Floor</p>
           <p>New York, New York 10004 US</p>
-          <p className='pt-4'>For Media Inquiries call 202-370-333</p>
+          <p className='mt-6'>For Media Inquiries call 202-370-333</p>
         </div>
       </section>
       <nav className='footer-links flex gap-10 content-center justify-center pb-4'>
